a85637d1dd0453ba7336e50794127951aed043d6
Alamin934 <islamalamin934@gmail.com>
2021-11-12 15:57:10 +0600

    dashboard style

--- src/Pages/Dashboard/DashBoard/Dashboard.js
+++ src/Pages/Dashboard/DashBoard/Dashboard.js
@@ -30,53 +30,57 @@ const Dashboard = () => {
     return (
         <div>
             <div className="p-3 p-md-5">
-                <Row className="g-4">
-                    <Col xs={12} md={3} className="border-end border-secondary border-2">
-                        <Nav className="flex-column text-md-start text-center">
-                            <NavLink to='/home' style={linkStyle}>Home</NavLink>
-                            {admin && <div className="d-flex flex-column">
-                                <NavLink to={`${url}/manageAllOrders`} style={linkStyle}>Manage All Orders</NavLink>
-                                <NavLink to={`${url}/addBiCycle`} style={linkStyle}>Add A BiCycle</NavLink>
-                                <NavLink to={`${url}/manageBiCycle`} style={linkStyle}>Manage BiCycle</NavLink>
-                                <NavLink to={`${url}/makeAdmin`} style={linkStyle}>Make Admin</NavLink>
-                            </div>}
+                <Row className="g-4 bg-info p-4 pb-5 rounded shadow-lg h-auto">
+                    <Col xs={12} md={3}>
+                        <div className="border-end border-dark border-2 bg-light p-4 rounded shadow">
+                            <Nav className="flex-column text-md-start text-center">
+                                <NavLink to='/home' style={linkStyle}>Home</NavLink>
+                                {admin && <div className="d-flex flex-column">
+                                    <NavLink to={`${url}/manageAllOrders`} style={linkStyle}>Manage All Orders</NavLink>
+                                    <NavLink to={`${url}/addBiCycle`} style={linkStyle}>Add A BiCycle</NavLink>
+                                    <NavLink to={`${url}/manageBiCycle`} style={linkStyle}>Manage BiCycle</NavLink>
+                                    <NavLink to={`${url}/makeAdmin`} style={linkStyle}>Make Admin</NavLink>
+                                </div>}
 
-                            <NavLink to={`${url}/myOrders`} style={linkStyle}>My Orders</NavLink>
-                            <NavLink to={`${url}/pay`} style={linkStyle}>Pay</NavLink>
-                            <NavLink to={`${url}/addReview`} style={linkStyle}>Add Review</NavLink>
-                            <div>
-                                <Button onClick={handleLogOut} variant="info" className="text-white fw-bold mt-2">Log Out</Button>
-                            </div>
-                        </Nav>
+                                <NavLink to={`${url}/myOrders`} style={linkStyle}>My Orders</NavLink>
+                                <NavLink to={`${url}/pay`} style={linkStyle}>Pay</NavLink>
+                                <NavLink to={`${url}/addReview`} style={linkStyle}>Add Review</NavLink>
+                                <div>
+                                    <Button onClick={handleLogOut} variant="info" className="text-white fw-bold mt-2">Log Out</Button>
+                                </div>
+                            </Nav>
+                        </div>
                     </Col>
                     <Col xs={12} md={9}>
-                        <Switch>
-                            {admin && <AdminRoute exact path={path}>
-                                <ManageAllOrders></ManageAllOrders>
-                            </AdminRoute>}
-                            <AdminRoute path={`${path}/manageAllOrders`}>
-                                <ManageAllOrders></ManageAllOrders>
-                            </AdminRoute>
-                            <AdminRoute path={`${path}/addBiCycle`}>
-                                <AddBiCycle></AddBiCycle>
-                            </AdminRoute>
-                            <AdminRoute path={`${path}/manageBiCycle`}>
-                                <ManageBiCycle></ManageBiCycle>
-                            </AdminRoute>
-                            <AdminRoute path={`${path}/makeAdmin`}>
-                                <MakeAdmin></MakeAdmin>
-                            </AdminRoute>
+                        <div className="p-3 bg-light h-auto rounded shadow">
+                            <Switch>
+                                {admin && <AdminRoute exact path={path}>
+                                    <ManageAllOrders></ManageAllOrders>
+                                </AdminRoute>}
+                                <AdminRoute path={`${path}/manageAllOrders`}>
+                                    <ManageAllOrders></ManageAllOrders>
+                                </AdminRoute>
+                                <AdminRoute path={`${path}/addBiCycle`}>
+                                    <AddBiCycle></AddBiCycle>
+                                </AdminRoute>
+                                <AdminRoute path={`${path}/manageBiCycle`}>
+                                    <ManageBiCycle></ManageBiCycle>
+                                </AdminRoute>
+                                <AdminRoute path={`${path}/makeAdmin`}>
+                                    <MakeAdmin></MakeAdmin>
+                                </AdminRoute>
 
-                            <Route path={`${path}/myOrders`}>
-                                <MyOrders></MyOrders>
-                            </Route>
-                            <Route path={`${path}/pay`}>
-                                <Pay></Pay>
-                            </Route>
-                            <Route path={`${path}/addReview`}>
-                                <AddReview></AddReview>
-                            </Route>
-                        </Switch>
+                                <Route path={`${path}/myOrders`}>
+                                    <MyOrders></MyOrders>
+                                </Route>
+                                <Route path={`${path}/pay`}>
+                                    <Pay></Pay>
+                                </Route>
+                                <Route path={`${path}/addReview`}>
+                                    <AddReview></AddReview>
+                                </Route>
+                            </Switch>
+                        </div>
                     </Col>
                 </Row>
             </div>
